ccfce015b831f72d65a6e29179f348b3160a8340
jyoti9420 <161121976+jyoti9420@users.noreply.github.com>
2024-03-16 10:38:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,225 @@
+constbtn= new Audio("Jeene Laga Hoon (Ramaiya Vastavaiya)-(PagalWorld.Ink) (4).mp3");
+document.querySelector(".btn1").addEventListener("click",function(){
+btn1.play();
+});
+document.querySelector(".btn2").addEventListener("click",function(){
+btn2.pause();
+});
+constbtn= new Audio("Piya-O-Re-Piya(PaglaSongs).mp3");
+document.querySelector(".btn3").addEventListener("click",function(){
+btn3.play();
+});
+document.querySelector(".btn4").addEventListener("click",function(){
+btn4.pause();
+});
+constbtn= new Audio("Tera-Hone-Laga-Hoon(PaglaSongs) (1).mp3");
+document.querySelector(".btn5").addEventListener("click",function(){
+btn5.play();
+});
+document.querySelector(".btn6").addEventListener("click",function(){
+btn6.pause();
+});
+constbtn= new Audio("");
+document.querySelector(".btn7").addEventListener("click",function(){
+btn7.play();
+});
+document.querySelector(".btn8").addEventListener("click",function(){
+btn8.pause();
+});
+constbtn= new Audio("Alvida-Ho-Gaye-Dekhte-Dekhte(PaglaSongs).mp3");
+document.querySelector(".btn9").addEventListener("click",function(){
+btn9.play();
+});
+document.querySelector(".btn10").addEventListener("click",function(){
+btn10.pause();
+});
+constbtn= new Audio("Agar-Tum-Saath-Ho---Tamasha(PagalWorld).mp3");
+document.querySelector(".btn11").addEventListener("click",function(){
+btn11.play();
+});
+document.querySelector(".btn12").addEventListener("click",function(){
+btn12.pause();
+});
+constbtn= new Audio("Thodi-Jagah(PagalWorld).mp3");
+document.querySelector(".btn13").addEventListener("click",function(){
+btn13.play();
+});
+document.querySelector(".btn14").addEventListener("click",function(){
+btn14.pause();
+});
+constbtn= new Audio("Tera_Chehra_-_Sanam_Teri_Kasam_128_Kbps.mp3");
+document.querySelector(".btn15").addEventListener("click",function(){
+btn15.play();
+});
+document.querySelector(".btn16").addEventListener("click",function(){
+btn16.pause();
+});
+constbtn= new Audio("Heeriye(PaglaSongs).mp3");
+document.querySelector(".btn17").addEventListener("click",function(){
+btn17.play();
+});
+document.querySelector(".btn18").addEventListener("click",function(){
+btn18.pause();
+});
+constbtn= new Audio("Khairiyat-Pucho(PaglaSongs).mp3");
+document.querySelector(".btn19").addEventListener("click",function(){
+btn19.play();
+});
+document.querySelector(".btn20").addEventListener("click",function(){
+btn20.pause();
+});
+constbtn= new Audio("Hawayein(PaglaSongs).mp3");
+document.querySelector(".btn21").addEventListener("click",function(){
+btn21.play();
+});
+document.querySelector(".btn22").addEventListener("click",function(){
+btn22.pause();
+});
+constbtn= new Audio("Hawayein(PaglaSongs).mp3");
+document.querySelector(".btn23").addEventListener("click",function(){
+btn23.play();
+});
+document.querySelector(".btn24").addEventListener("click",function(){
+btn24.pause();
+});
+constbtn= new Audio("Sirhaana(PaglaSongs).mp3");
+document.querySelector(".btn25").addEventListener("click",function(){
+btn25.play();
+});
+document.querySelector(".btn26").addEventListener("click",function(){
+btn26.pause();
+});
+constbtn= new Audio("Perfy - Paradox-(DJMaza).mp3");
+document.querySelector(".btn27").addEventListener("click",function(){
+btn27.play();
+});
+document.querySelector(".btn28").addEventListener("click",function(){
+btn28.pause();
+});
+constbtn= new Audio("Babam-Bam(PaglaSongs).mp3");
+document.querySelector(".btn29").addEventListener("click",function(){
+btn29.play();
+});
+document.querySelector(".btn30").addEventListener("click",function(){
+btn30.pause();
+});
+constbtn= new Audio("Cowboy_-_Hustle_2_0_128_Kbps.mp3");
+document.querySelector(".btn31").addEventListener("click",function(){
+btn31.play();
+});
+document.querySelector(".btn32").addEventListener("click",function(){
+btn32.pause();
+});
+constbtn= new Audio("Hasti-Rahe-Tu(PaglaSongs).mp3");
+document.querySelector(".btn33").addEventListener("click",function(){
+btn33.play();
+});
+document.querySelector(".btn34").addEventListener("click",function(){
+btn34.pause();
+});
+constbtn= new Audio("Jaadugar(PaglaSongs) (1).mp3");
+document.querySelector(".btn35").addEventListener("click",function(){
+btn35.play();
+});
+document.querySelector(".btn36").addEventListener("click",function(){
+btn36.pause();
+});
+constbtn= new Audio("Raat_Bhar_-_Heropanti_128_Kbps.mp3");
+document.querySelector(".btn37").addEventListener("click",function(){
+btn37.play();
+});
+document.querySelector(".btn38").addEventListener("click",function(){
+btn38.pause();
+});
+constbtn= new Audio("Guli-Mata(PaglaSongs).mp3");
+document.querySelector(".btn39").addEventListener("click",function(){
+btn39.play();
+});
+document.querySelector(".btn40").addEventListener("click",function(){
+btn40.pause();
+});
+constbtn= new Audio("Raabta(PaglaSongs).mp3");
+document.querySelector(".btn41").addEventListener("click",function(){
+btn41.play();
+});
+document.querySelector(".btn42").addEventListener("click",function(){
+btn42.pause();
+});
+constbtn= new Audio("Ve Kamleya 128 Kbps.mp3");
+document.querySelector(".btn43").addEventListener("click",function(){
+btn43.play();
+});
+document.querySelector(".btn44").addEventListener("click",function(){
+btn44.pause();
+});
+constbtn= new Audio("Zihaal-e-Miskin(PaglaSongs).mp3");
+document.querySelector(".btn45").addEventListener("click",function(){
+btn45.play();
+});
+document.querySelector(".btn46").addEventListener("click",function(){
+btn46.pause();
+});
+constbtn= new Audio("Tere-Hawale(PaglaSongs).mp3");
+document.querySelector(".btn47").addEventListener("click",function(){
+btn47.play();
+});
+document.querySelector(".btn48").addEventListener("click",function(){
+btn48.pause();
+});
+constbtn= new Audio("");
+document.querySelector(".btn49").addEventListener("click",function(){
+btn49.play();
+});
+document.querySelector(".btn50").addEventListener("click",function(){
+btn50.pause();
+});
+constbtn= new Audio("");
+document.querySelector(".btn51").addEventListener("click",function(){
+btn51.play();
+});
+document.querySelector(".btn52").addEventListener("click",function(){
+btn52.pause();
+});
+constbtn= new Audio("Haseena(PagalWorld).mp3");
+document.querySelector(".btn53").addEventListener("click",function(){
+btn53.play();
+});
+document.querySelector(".btn54").addEventListener("click",function(){
+btn54.pause();
+});
+constbtn= new Audio("Recovery Song By Kayden Sharma.mp3");
+document.querySelector(".btn55").addEventListener("click",function(){
+btn55.play();
+});
+document.querySelector(".btn56").addEventListener("click",function(){
+btn56.pause();
+});
+constbtn= new Audio("");
+document.querySelector(".btn57").addEventListener("click",function(){
+btn57.play();
+});
+document.querySelector(".btn58").addEventListener("click",function(){
+btn58.pause();
+});
+constbtn= new Audio("Mehbooba---Preet-Bandre(PagalWorld).mp3");
+document.querySelector(".btn59").addEventListener("click",function(){
+btn59.play();
+});
+document.querySelector(".btn60").addEventListener("click",function(){
+btn60.pause();
+});
+constbtn= new Audio("Navari-Sadi-Pahije(PaglaSongs).mp3");
+document.querySelector(".btn61").addEventListener("click",function(){
+btn61.play();
+});
+document.querySelector(".btn62").addEventListener("click",function(){
+btn62.pause();
+});
+
+
+
+
+
+
+
+
